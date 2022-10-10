e0258d99d608c6c5e542a73531396dc8a7ff83b8
Rajendra-Chozhan <rajchozhan024@gmail.com>
2022-10-10 13:23:53 +0530

    Update

---
+++ src/arrays/Sorting.java
@@ -0,0 +1,33 @@
+package arrays;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		
+		
+		int [] marks = new int[]{42,88,79,65,52,49,39};
+		
+		
+		int temp;
+		
+		for (int i = 0;i<marks.length;i++) {
+			
+	for (int j =i;j<marks.length;j++) {		
+			
+		if (marks[i] > marks[j]){
+		
+			temp = marks[i];
+			marks[i] = marks[j];
+			marks[j] = temp;
+			
+	
+		}
+
+	}}
+		
+for (int a : marks) {
+	System.out.println(marks);
+	
+}
+
+}}
